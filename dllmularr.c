081d2d8c0e26a0ff092994b117244061148f8029
Rishikesh B Nair <rishikeshbnair@gmail.com>
2026-01-21 08:28:43 +0530

    Add files via upload

--- dllmularr.c
+++ dllmularr.c
@@ -18,23 +18,23 @@ int getfreeindex() {
 
 void insertend(int x){
     int index, temp;
-    index= getfreeindex();
+    index = getfreeindex();
     if(index == -1){
         printf("no free space to insert \n");
     }else{
-        data[index]=x;
-        next[index]=-1;
+        data[index] = x;
+        next[index] = -1;
         if(head == -1){
             head = index;
             prev[index] = -1;
             tail = index;
         }else{
-            temp=head;
-            while(next[temp]!=-1){
-                temp=next[temp];
+            temp = head;
+            while(next[temp] != -1){
+                temp = next[temp];
             }
-            next[temp]=index;
-            prev[index]=temp;
+            next[temp] = index;
+            prev[index] = temp;
             tail = index;
         }
     }
@@ -42,123 +42,128 @@ void insertend(int x){
 
 void insertbegin(int x){
     int index;
-    index= getfreeindex();
+    index = getfreeindex();
     if(index == -1){
         printf("no free space to insert \n");
     }else{
-        data[index]=x;
-        prev[index]=-1;
+        data[index] = x;
+        prev[index] = -1;
         if(head == -1){
             head = index;
-            next[index]=-1;
+            next[index] = -1;
+            tail = index;         
         }else{
-            next[index]=head;
-            prev[head]=index;
-            head=index;
+            next[index] = head;
+            prev[head] = index;
+            head = index;
         }
     }
 }
 
-void deleteend()
-{
+void deleteend(){
     int temp;
-    if(head==-1){
+    if(head == -1){
         printf("empty\n");
     }else{
-        temp=head;
-        while(next[temp]!=-1){
-            temp=next[temp];
+        temp = head;
+        while(next[temp] != -1){
+            temp = next[temp];
         }
-        data[temp]=-1;
-        next[temp]=-1;
-        prev[temp]=-1;
-        tail=prev[temp];
-    }
-}
-void deletebegin()
-{
-    int temp;
-    if(head==-1){
-        printf("empty\n");
-    }else{
-        temp=head;
-        head=next[head];
-        if(head!=-1){
-            prev[head]=-1;
-        }
-        data[temp]=-1;
-        next[temp]=-1;
-        prev[temp]=-1;
+        if(prev[temp] != -1)
+            next[prev[temp]] = -1;
+        else
+            head = -1;
+
+        tail = prev[temp];      
+
+        data[temp] = -1;
+        next[temp] = -1;
+        prev[temp] = -1;
     }
 }
 
-void display()
-{
+void deletebegin(){
     int temp;
-    temp=head;
-    if (temp==-1){
+    if(head == -1){
+        printf("empty\n");
+    }else{
+        temp = head;
+        head = next[head];
+        if(head != -1){
+            prev[head] = -1;
+        }else{
+            tail = -1;            
+        }
+        data[temp] = -1;
+        next[temp] = -1;
+        prev[temp] = -1;
+    }
+}
+
+void display(){
+    int temp;
+    temp = head;
+    if(temp == -1){
         printf("empty\n");
     }else{
         printf("data;\n");
-        while(temp!=-1){
-            if(data[temp]!=-1){
+        while(temp != -1){
+            if(data[temp] != -1){
                 printf("%d ", data[temp]);
             }
-            temp=next[temp];
+            temp = next[temp];
         }
         printf("\n");
     }
 }
 
-void fdisplay()
-{
+void fdisplay(){
     int temp;
-    temp=tail;
-    if (temp==-1){
+    temp = tail;
+    if(temp == -1){
         printf("empty\n");
     }else{
         printf("data in reverse order:\n");
-        while(temp!=-1){
-            if(data[temp]!=-1){
+        while(temp != -1){
+            if(data[temp] != -1){
                 printf("%d ", data[temp]);
             }
-            temp=prev[temp];
+            temp = prev[temp];
         }
         printf("\n");
     }
 }
 
-void main(){
-    for(int i=0; i<20; i++){
-        data[i]=-1;
-        next[i]=-1;
-        prev[i]=-1;
+int main(){
+    for(int i = 0; i < 20; i++){
+        data[i] = -1;
+        next[i] = -1;
+        prev[i] = -1;
     }
-    char ch='y';
-    while(ch=='y'){
+
+    while(1){
         int choice;
         printf("1.insertend \n2.insertbegin \n3.deleteend \n4.deletebegin \n5.display \n6.fdisplay \n7.exit \n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
+
         switch(choice){
-            case 1:
-                {
-                    int val;
-                    printf("Enter value to insert at end: ");
-                    scanf("%d", &val);
-                    insertend(val);
-                    display();
-                    break;
-                }
-            case 2:
-                {
-                    int val;
-                    printf("Enter value to insert at beginning: ");
-                    scanf("%d", &val);
-                    insertbegin(val);
-                    display();
-                    break;
-                }
+            case 1:{
+                int val;
+                printf("Enter value to insert at end: ");
+                scanf("%d", &val);
+                insertend(val);
+                display();
+                break;
+            }
+            case 2:{
+                int val;
+                printf("Enter value to insert at beginning: ");
+                scanf("%d", &val);
+                insertbegin(val);
+                display();
+                break;
+            }
             case 3:
                 deleteend();
                 display();
@@ -175,10 +180,8 @@ void main(){
                 break;
             case 7:
                 exit(0);
-                break;
             default:
                 printf("Invalid choice\n");
         }
-        
     }
 }
--- dllsingarr.c
+++ dllsingarr.c
@@ -1,113 +1,139 @@
 #include <stdio.h>
 
 int array[100];
-int head=-1;
-int tail=-1;
+int head = -1;
+int tail = -1;
 
-int getfreeindex(){
-    for(int i=0;i<100;i+=3){
-        if(array[i]==-1 && array[i+1]==-1 && array[i+2]==-1)
+int getfreeindex()
+{
+    for (int i = 0; i < 100; i += 3)
+    {
+        if (array[i] == -1 && array[i + 1] == -1 && array[i + 2] == -1)
             return i;
     }
     return -1;
 }
 
-void insertend(int x){
-    int index=getfreeindex();
-    if(index==-1){
+void insertend(int x)
+{
+    int index = getfreeindex();
+    if (index == -1)
+    {
         printf("No free space to insert\n");
         return;
     }
 
-    array[index+1]=x;
-    array[index+2]=-1;
+    array[index + 1] = x;
+    array[index + 2] = -1;
 
-    if(head==-1){
-        head=index;
-        array[index]=-1;
-        tail=index;
-    }else{
-        int temp=head;
-        while(array[temp+2]!=-1)
-            temp=array[temp+2];
-        array[temp+2]=index;
-        array[index]=temp;
-        tail=index;
+    if (head == -1)
+    {
+        head = index;
+        array[index] = -1;
+        tail = index;
+    }
+    else
+    {
+        int temp = head;
+        while (array[temp + 2] != -1)
+            temp = array[temp + 2];
+        array[temp + 2] = index;
+        array[index] = temp;
+        tail = index;
     }
 }
 
-void deleteendd(){
+void deleteendd()
+{
     int temp;
-    if (head==-1){
+    if (head == -1)
+    {
         printf("List is empty\n");
         return;
-    }else{
-        temp=head;
-        if(array[temp+2]==-1){
-            head=-1;
-            array[temp]=-1;
-            array[temp+1]=-1;
-            array[temp+2]=-1;
+    }
+    else
+    {
+        temp = head;
+        if (array[temp + 2] == -1)
+        {
+            head = -1;
+            tail = -1;
+            array[temp] = -1;
+            array[temp + 1] = -1;
+            array[temp + 2] = -1;
             return;
-        }else{
-            while(array[temp+2]!=-1){
-                temp=array[temp+2];
-            }
-            array[temp]=-1;
-            array[temp+1]=-1;
-            array[temp+2]=-1;
-        
+        }
+        else
+        {
+            while (array[temp + 2] != tail)
+                temp = array[temp + 2];
+
+            array[tail] = -1;
+            array[tail + 1] = -1;
+            array[tail + 2] = -1;
+
+            array[temp + 2] = -1;
+            tail = temp;
         }
     }
 }
 
-void display(){
-    if(head==-1){
+void display()
+{
+    if (head == -1)
+    {
         printf("List is empty\n");
         return;
     }
 
-    int temp=head;
+    int temp = head;
     printf("Data: ");
-    while(temp!=-1){
-        if(array[temp+1]!=-1){
-        printf("%d ",array[temp+1]);
-        }temp=array[temp+2];
+    while (temp != -1)
+    {
+        if (array[temp + 1] != -1)
+        {
+            printf("%d ", array[temp + 1]);
+        }
+        temp = array[temp + 2];
     }
     printf("\n");
 }
 
-int main(){
-    for(int i=0;i<100;i+=3){
-        array[i]=-1;
-        array[i+1]=-1;
-        array[i+2]=-1;
+int main()
+{
+    for (int i = 0; i < 100; i += 3)
+    {
+        array[i] = -1;
+        array[i + 1] = -1;
+        array[i + 2] = -1;
     }
 
-    int choice,val;
-    while(1){
+    int choice, val;
+    while (1)
+    {
         printf("\n1.Insert End\n2.Delete End\n3.display\n4.exit\n");
         printf("Enter your choice: ");
-        scanf("%d",&choice);
+        scanf("%d", &choice);
 
-        switch(choice){
-            case 1:
-                printf("Value to insert: ");
-                scanf("%d",&val);
-                insertend(val);
-                display();
-                break;
-            case 2:
-                deleteendd();
-                display();
-                break;
-            case 3:
-                display();
-                break;
-            case 4:
-                return 0;
-            default:
-                printf("Invalid choice\n");
+        switch (choice)
+        {
+        case 1:
+            printf("Value to insert: ");
+            scanf("%d", &val);
+            insertend(val);
+            display();
+            break;
+        case 2:
+            deleteendd();
+            display();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            return 0;
+        default:
+            printf("Invalid choice\n");
         }
     }
 }
